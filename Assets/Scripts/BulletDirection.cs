7f448f51951817668828272e1731cba56eae3158
Thomas <thomas.picard@isen.yncrea.fr>
2021-11-02 15:56:07 +0100

    Shoot

--- Assets/Scripts/BulletDirection.cs
+++ Assets/Scripts/BulletDirection.cs
@@ -6,21 +6,31 @@ public class BulletDirection : MonoBehaviour
 {
 
     public GameObject target;
-    public float speed = 3.0f;
+    public float speed = 6f;
     private Vector3 targetPoint;
     private float offsettY = 1.5f;
+    private Vector3 _dir;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        transform.parent = null;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         target = GameObject.Find("Player");
-        targetPoint = new Vector3(target.transform.position.x, target.transform.position.y + offsettY, target.transform.position.z);
+        targetPoint = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+        _dir = (targetPoint - transform.position).normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, targetPoint, step);
+        //float step = speed * Time.deltaTime;
+        //transform.position = Vector3.MoveTowards(transform.position, targetPoint, step);
+        _rigidbody.MovePosition(_dir * speed * Time.deltaTime + transform.position);
     }
 }
--- Assets/Scripts/EnemyShot.cs
+++ Assets/Scripts/EnemyShot.cs
@@ -9,22 +9,31 @@ public class EnemyShot : MonoBehaviour
     private Vector3 targetSpawnBullet;
     private float offsetY = 1.25f;
     private float offsetX = -0.9f;
+    private float timerShoot = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
         targetSpawnBullet = new Vector3(transform.position.x+offsetX, transform.position.y + offsetY, transform.position.z);
-        StartCoroutine(InstanceBullet());
     }
 
+    void Update()
+    {
+        timerShoot -= Time.deltaTime;
+        if (timerShoot < 0)
+        {
+            InstanceBullet();
+            timerShoot = 2f;
+        }
+  
+    }
 
     // Update is called once per frame
-    IEnumerator InstanceBullet()
+    void InstanceBullet()
     {
-        while (DistanceEnemyPlayer() <= 7)
+        if (DistanceEnemyPlayer() <= 15)
         {
             Instantiate(prefab, targetSpawnBullet, Quaternion.identity);
-            yield return new WaitForSeconds(1);
         }
     }
 
